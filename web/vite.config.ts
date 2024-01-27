2a322bce90c31e70b835f3df229fe418abce077b
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-01-27 16:05:10 -0500

    chore(web): replace babel with swc

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
