0d26b88c51f3ec81b7340030b742f18b332989bd
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-01-27 16:47:34 -0500

    chore(baseplate): initial commit

---
+++ pkg/baseplate/doc.go
@@ -0,0 +1,7 @@
+// Package baseplate provides a batteries-included starting point for go services.
+//
+// This package provides convenient initialization bundles needed in your main
+// function, and initialize the subpackages for you.
+// For concrete features provided by Baseplate.go packages,
+// please refer to the documentation in subdirectories.
+package baseplate
