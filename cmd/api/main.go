c8911f03e70fe21323f759ba9d01a65bd794305c
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-04-13 22:34:58 -0400

    chore: scaffold go api server with first route (#1)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+	"runtime/debug"
+	"sync"
+)
+
+type config struct {
+	baseURL  string
+	httpPort int
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
 
 func main() {
-	fmt.Println("Not Implemented, yet.")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	err := run(logger)
+	if err != nil {
+		trace := string(debug.Stack())
+		logger.Error(err.Error(), "trace", trace)
+		os.Exit(1)
+	}
+}
+
+func run(logger *slog.Logger) error {
+	var cfg config
+	cfg.baseURL = "http://localhost" // env.GetInt("BASE_URL", "http://localhost")
+	cfg.httpPort = 4000              // env.GetInt("HTTP_PORT", 4000)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	return app.serveHTTP()
 }
