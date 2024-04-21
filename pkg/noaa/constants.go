16698afe79992b5c759c8ede203fa97f0d71cb7f
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-04-21 11:05:15 -0400

    chore(noaa): remove trailing slash from base url

--- pkg/noaa/constants.go
+++ pkg/noaa/constants.go
@@ -3,17 +3,17 @@ package noaa
 // NDBC Endpoints
 const (
 	// NDBC Base URL
-	baseUrlNDBC = "https://www.ndbc.noaa.gov/"
+	baseUrlNDBC = "https://www.ndbc.noaa.gov"
 
-	ActiveStations = baseUrlNDBC + "activestations"
-	Realtime       = baseUrlNDBC + "data/realtime2"
+	ActiveStations = baseUrlNDBC + "/activestations"
+	Realtime       = baseUrlNDBC + "/data/realtime2"
 )
 
 // NOAA Endpoints
 const (
 	// NOAA Base URL
-	baseUrlNOAA = "https://graphical.weather.gov/"
+	baseUrlNOAA = "https://graphical.weather.gov"
 
 	// National Digital Forecast Database
-	Forecasts = baseUrlNOAA + "xml/sample_products/browser_interface/ndfdXMLclient.php"
+	Forecasts = baseUrlNOAA + "/xml/sample_products/browser_interface/ndfdXMLclient.php"
 )
