9ab803cf565bc51f76bfe99e5a4c9d91b44068d4
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2024-12-27 15:58:05 -0500

    test(web): update imports to use `vitest`

--- web/src/app.test.tsx
+++ web/src/app.test.tsx
@@ -1,5 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
 
 import App from './app';
 
